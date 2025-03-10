c1303071019d2cfa970fc38825804b28a45ebc7e
Katia <katia.lovchinskaya@gmail.com>
2025-03-10 08:39:07 +0200

    fixes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,21 @@
 import css from "./Contact.module.css";
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { FaUser } from 'react-icons/fa';
 import { BsFillTelephoneFill } from 'react-icons/bs';
 
 
-export default function Contact ({contacts, forDelete}) {
+export default function Contact ({contacts}) {
 
     return (
-        <div className={css.contactinfo}>
-            {contacts.map(({id, name, number }) =>
-            <div className={css.info} key={nanoid()}>
-                <div className={css.inform}>
-                    <span className={css.contact}>
-                    <FaUser className={css.icon} />
-                        {name}</span>
-
-                    <span className={css.contact}>
-                    <BsFillTelephoneFill className={css.icon} />
-                    {number}</span>
-                </div>
-                <button className={css.buttondelete} onClick={() => forDelete(id)}><span>Delete</span></button>
-            </div>)}
-            <div>{contacts.length === 0 && "Your Contacts Folder is empty!"}</div>
-        </div>
+    <div className={css.inform}>
+                            <span className={css.contact}>
+                            <FaUser className={css.icon} />
+                                {contacts.name}</span>
+        
+                            <span className={css.contact}>
+                            <BsFillTelephoneFill className={css.icon} />
+                            {contacts.number}</span>
+                        </div>
 
     )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,21 @@
+import css from '../ContactList/ContactList.module.css'
 import Contact from '../Contact/Contact'
-
+import { nanoid } from 'nanoid';
 
 export default function ContactList ({contacts, onDelete}){
-    
+    // let id=nanoid();
     return (
-        <div>
-            <Contact contacts={contacts} forDelete={onDelete}/>
-        </div>
+        // <div>
+        //     <Contact contacts={contacts} forDelete={onDelete}/>
+        // </div>
+         
+        <div className={css.contactinfo}>
+                    {contacts.map((contacts) =>
+                    <div className={css.info} key={nanoid()}>
+                        <Contact contacts={contacts}/>
+                        <button className={css.buttondelete} onClick={() => onDelete(contacts.id)}><span>Delete</span></button> 
+                    </div>)}
+                    <div>{contacts.length === 0 && "Your Contacts Folder is empty!"}</div>
+                </div>
     )
 }
